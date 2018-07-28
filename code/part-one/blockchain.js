bdf16ec40bd21d2d5e80d13169b457c2e14d9c72
David Seidenberg <davidseidenberg@gmail.com>
2018-07-28 11:00:59 -0700

    Stuck verifying signature and signed message in transaction class

--- code/part-one/blockchain.js
+++ code/part-one/blockchain.js
@@ -23,7 +23,11 @@ class Transaction {
    */
   constructor(privateKey, recipient, amount) {
     // Enter your solution here
-
+    this.source = signing.getPublicKey(privateKey);
+    this.recipient = recipient;
+    this.amount = amount;
+    this.signature = signing.sign(privateKey, recipient + amount);
+    console.log(this.signature.length);
   }
 }
 
